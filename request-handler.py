24a703e11c2475b16b6917409376378493668bdc
Hannah Hall <hannahkhall13@gmail.com>
2022-01-10 12:57:05 -0600

    Ch 1-2: request boilerplate code

---
+++ request-handler.py
@@ -0,0 +1,65 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+from views import get_all_animals
+
+
+# Here's a class. It inherits from another class.
+# For now, think of a class as a container for functions that
+# work together for a common purpose. In this case, that
+# common purpose is to respond to HTTP requests from a client.
+class HandleRequests(BaseHTTPRequestHandler):
+    def _set_headers(self, status):
+        # Notice this Docstring also includes information about the arguments passed to the function
+        """Sets the status code, Content-Type and Access-Control-Allow-Origin
+        headers on the response
+
+        Args:
+            status (number): the status code to return to the front end
+        """
+        self.send_response(status)
+        self.send_header('Content-type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+
+    # Another method! This supports requests with the OPTIONS verb.
+    def do_OPTIONS(self):
+        """Sets the options headers
+        """
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+        self.send_header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept')
+        self.end_headers()
+
+    def do_GET(self):
+        self._set_headers(200)
+
+        print(self.path)
+
+        if self.path == '/animals':
+            response = get_all_animals()
+        else:
+            response = []
+
+        self.wfile.write(f"{response}".encode())
+
+    def do_POST(self):
+        self._set_headers(201)
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        response = f'received request: {post_body}'
+        self.wfile.write(response.encode())
+
+    def do_PUT(self):
+        self.do_POST()
+
+def main():
+    """Starts the server on port 8088 using the HandleRequests class
+    """
+    host = ''
+    port = 8088
+    HTTPServer((host, port), HandleRequests).serve_forever()
+
+
+if __name__ == "__main__":
+    main()
