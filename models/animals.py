b337ddcde89b59138e25cb8cb424b2a600338831
hannahhall <hannahkhall13@gmail.com>
2022-01-13 11:57:31 -0600

    ch 7 - Models

---
+++ models/animals.py
@@ -0,0 +1,9 @@
+class Animal:
+    def __init__(self, id, name, breed, status, location_id, customer_id=1):
+        self.id = id
+        self.name = name
+        self.breed = breed
+        self.status = status
+        self.location_id = location_id
+        self.customer_id = customer_id
+
